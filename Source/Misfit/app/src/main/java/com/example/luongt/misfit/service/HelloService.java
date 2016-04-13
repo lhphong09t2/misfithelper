f4d7878b4266928be2250acf124ecbdc0d604e19
Tuyet Luong <tuyet.luong@sioux.asia>
2016-04-13 14:52:17 +0700

    Revert "[Source] Fix bug: exit setting mode when unlock"
    This reverts commit b1c2b5d1ceada6dfcbbe3c1ee724538444d02b08.

--- Source/Misfit/app/src/main/java/com/example/luongt/misfit/service/HelloService.java
+++ Source/Misfit/app/src/main/java/com/example/luongt/misfit/service/HelloService.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
-import com.example.luongt.misfit.LockActivity;
 import com.example.luongt.misfit.MFContants;
 import com.example.luongt.misfit.MisfitEventNotifierApplication;
 import com.example.luongt.misfit.R;
@@ -112,12 +111,7 @@ public class HelloService extends TTSService implements MFLGestureCommandDelegat
                     speak(_currentMisfitHelper.getName());
                     break;
                 case "tp":
-                    if(LockActivity.getInstance() == null){
-                        startActivity(new Intent(this, LockActivity.class).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
-                    }
-                    else {
-                        LocalBroadcastManager.getInstance(this).sendBroadcast(new Intent(MFContants.FINISH_LOCK));
-                    }
+                    LocalBroadcastManager.getInstance(this).sendBroadcast(new Intent(MFContants.FINISH_LOCK));
                     break;
                 default:
                     _isSettingMode = false;
